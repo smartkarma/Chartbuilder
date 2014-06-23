fb1c0a0c8c3e1d748b89215c24a2d9e509f1a614
yanofsky <github@yerit.com>
2014-06-23 14:29:12 -0400

    first labels on everything

--- js/gneisschart.js
+++ js/gneisschart.js
@@ -1791,6 +1791,20 @@ function Gneiss(config)
 					.attr("y",function(d,i) {yAxisIndex = d3.select(this.parentNode).data()[0].axis; return (g.yAxis()[yAxisIndex].scale(d)-g.yAxis()[yAxisIndex].scale(Gneiss.helper.columnXandHeight(d,g.yAxis()[yAxisIndex].scale.domain()))) >= 0 ? g.yAxis()[yAxisIndex].scale(Gneiss.helper.columnXandHeight(d,g.yAxis()[yAxisIndex].scale.domain())) : g.yAxis()[yAxisIndex].scale(d)})
 				
 				columnRects.exit().remove()
+
+				//add labels to columns
+				columnGroups.selectAll("text")
+					.data(function(d){return d.data})
+					.enter()
+					.append("text")
+					.text(function(d){return d})
+					.attr("text-anchor","middle")
+					.attr("x",g.xAxis().type =="date" ? 
+							function(d,i) {return g.xAxis().scale(g.xAxisRef()[0].data[i])}:
+							function(d,i) {return g.xAxis().scale(i)}
+					)
+					.attr("y", function(d,i) {yAxisIndex = d3.select(this.parentNode).data()[0].axis; return d || d ===0 ? g.yAxis()[yAxisIndex].scale(d) : -100})
+
 			
 				//add lines
 				lineSeries = g.seriesContainer.selectAll("path")
@@ -1849,6 +1863,19 @@ function Gneiss(config)
 						})
 			
 				lineSeriesDots.exit().remove()
+
+
+				//add labels to data points
+				lineSeriesDotGroups.selectAll("text")
+					.data(function(d,i){return d.data})
+					.enter()
+					.append("text")
+					.text(function(d){return d})
+					.attr("transform",function(d,i){
+						yAxisIndex = d3.select(this.parentNode).data()[0].axis;
+							var y = d || d ===0 ? g.yAxis()[yAxisIndex].scale(d) : -100;
+							return "translate("+ g.xAxis().scale(g.xAxisRef()[0].data[i]) + "," + y + ")";
+						})
 								
 				//add scatter
 				scatterGroups = g.seriesContainer.selectAll("g.seriesScatter")
